98c57fc769e6c80d341cdf46dc5164f12b62bb93
Marcin_Fi <fiukmarcinn@gmail.com>
2023-02-03 09:22:56 +0000

    algorithm: averagePair

---
+++ averagePair/script.js
@@ -0,0 +1,34 @@
+/*
+Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+ */
+
+function averagePair(arr, target) {
+    if (!arr.length) {
+        return false;
+    }
+
+    let value = 0;
+    let next = 1;
+
+    while (arr.length > value) {
+        const temporaryAverage = (arr[value] + arr[next]) / 2;
+
+        if (temporaryAverage === target) {
+            return true;
+        }
+
+        next++;
+
+        if (arr.length < next) {
+            value++;
+            next = value + 1;
+        }
+    }
+
+    return false;
+}
+
+console.log(averagePair([1, 2, 3], 2.5)); // true
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // true
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); // false
+console.log(averagePair([], 4)); // false
