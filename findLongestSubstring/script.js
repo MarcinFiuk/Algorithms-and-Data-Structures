5aa591b21341e905c4f7debdfe31aa4218d9d31e
Marcin_Fi <fiukmarcinn@gmail.com>
2023-02-05 09:47:24 +0000

    algorithm findLongestSubstring

---
+++ findLongestSubstring/script.js
@@ -0,0 +1,38 @@
+/*
+Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
+*/
+
+function findLongestSubstring(str) {
+    let subStr = str[0];
+    let end = 1;
+    let length = 0;
+
+    if (!str.length) {
+        return 0;
+    }
+
+    while (end < str.length) {
+        if (subStr.includes(str[end])) {
+            const index = subStr.indexOf(str[end]);
+            subStr = subStr.slice(index + 1);
+        }
+
+        subStr += str[end];
+
+        if (subStr.length > length) {
+            length = subStr.length;
+        }
+
+        end++;
+    }
+
+    return length;
+}
+
+console.log(findLongestSubstring('')); // 0
+console.log(findLongestSubstring('rithmschool')); // 7
+console.log(findLongestSubstring('thisisawesome')); // 6
+console.log(findLongestSubstring('thecatinthehat')); // 7
+console.log(findLongestSubstring('bbbbbb')); // 1
+console.log(findLongestSubstring('longestsubstring')); // 8
+console.log(findLongestSubstring('thisishowwedoit')); // 6
