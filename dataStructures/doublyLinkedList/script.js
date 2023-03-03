0c9b0b198acef63810431a7dece8c5caeb1c16a7
Marcin_Fi <fiukmarcinn@gmail.com>
2023-03-03 20:02:25 +0000

    doublyLinkedList - new methods

--- dataStructures/doublyLinkedList/script.js
+++ dataStructures/doublyLinkedList/script.js
@@ -122,16 +122,91 @@ class DoublyLinkedList {
 
         return false;
     }
+
+    insert(index, val) {
+        if (index < 0 || index > this.length) {
+            return false;
+        }
+
+        if (index === 0) {
+            return Boolean(this.unshift(val));
+        }
+
+        if (index === this.length) {
+            return Boolean(this.push(val));
+        }
+
+        const newNode = new Node(val);
+        const prevNode = this.get(index - 1);
+
+        newNode.next = prevNode.next;
+        newNode.next.prev = newNode;
+        newNode.prev = prevNode;
+        prevNode.next = newNode;
+
+        this.length++;
+
+        return true;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) {
+            return;
+        }
+
+        if (index === 0) {
+            return this.shift();
+        }
+        if (index === this.length - 1) {
+            return this.pop();
+        }
+
+        const removedNode = this.get(index);
+
+        removedNode.prev.next = removedNode.next;
+        removedNode.next.prev = removedNode.prev;
+        removedNode.prev = null;
+        removedNode.next = null;
+
+        this.length--;
+        return removedNode;
+    }
+
+    reverse() {
+        let current = this.tail;
+        this.tail = this.head;
+        this.head = current;
+
+        for (let i = 0; i < this.length; i++) {
+            const temp = current.next;
+            current.next = current.prev;
+            current.prev = temp;
+            current = current.next;
+        }
+
+        return this;
+    }
+
+    print() {
+        const arr = [];
+        let current = this.head;
+        while (current) {
+            arr.push(current.val);
+            current = current.next;
+        }
+
+        console.log(arr);
+    }
 }
 
 const dList = new DoublyLinkedList();
 
 dList.push(10);
-dList.push(20);
-dList.push(30);
-dList.push(40);
-dList.push(50);
+// dList.push(20);
+// dList.push(30);
+// dList.push(40);
+// dList.push(50);
+console.log(dList.print());
 
-const r = dList.set(-1, 'bpb');
-console.log(r);
-console.log(dList);
+dList.reverse();
+console.log(dList.print());
